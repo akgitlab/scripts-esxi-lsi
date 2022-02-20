81315d934e4f95cf2877d3f5aa835c775f44c1b8
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-02-20 18:50:40 +0500

    Update lsimr-pd-info-all.sh

--- hardware/lsi/scripts/lsimr-pd-info-all.sh
+++ hardware/lsi/scripts/lsimr-pd-info-all.sh
@@ -3,8 +3,9 @@
 #Andrey Kuznetsov, 2022.02.20
 
 # Get variables
-HOST=$1
+HVUID=$1
 PDNAME=$2
+HOST=$(grep -w $HVUID /etc/zabbix/scripts/lsimr-hosts-info | awk '{print $NF}')
 
 # We form the data of the received controller
 CTLSTR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST '/opt/lsi/storcli/storcli $PDNAME show all j'"
