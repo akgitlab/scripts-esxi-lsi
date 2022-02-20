9555fd3f21300e69e8da66ed8e1c15746fc70e72
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-02-20 10:51:55 +0500

    Update lsimr-pd-info-all.sh

--- hardware/lsi/scripts/lsimr-pd-info-all.sh
+++ hardware/lsi/scripts/lsimr-pd-info-all.sh
@@ -3,9 +3,9 @@
 #Andrey Kuznetsov, 2022.02.10
 
 # Get variables
-CLIIP=$1
+HOST=$1
 PDNAME=$2
 
 # We form the data of the received controller
-CTLSTR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$CLIIP '/opt/lsi/storcli/storcli $PDNAME show all j'"
+CTLSTR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST '/opt/lsi/storcli/storcli $PDNAME show all j'"
 echo $CTLSTR | bash
