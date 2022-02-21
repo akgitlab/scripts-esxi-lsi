75b0198f64a01d5e1c432c9a228ef7f051162cf4
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-02-21 09:03:04 +0500

    Update lsimr-pd-info-all.sh

--- hardware/lsi/scripts/lsimr-pd-info-all.sh
+++ hardware/lsi/scripts/lsimr-pd-info-all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 #Collecting data from LSI MegaRAID controller
 #Andrey Kuznetsov, 2022.02.20
 
 # Get variables
 HVUID=$1
 PDNAME=$2
-HOST=$(grep -w $HVUID /etc/zabbix/scripts/lsimr-hosts-info | awk '{print $NF}')
+HOST=$(grep -w $HVUID /etc/zabbix/scripts/lsimr-hosts-list.txt | awk '{print $NF}')
 
 # We form the data of the received controller
 CTLSTR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST '/opt/lsi/storcli/storcli $PDNAME show all j'"
