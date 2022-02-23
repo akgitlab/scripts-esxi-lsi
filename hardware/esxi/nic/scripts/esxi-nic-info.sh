df103e6136601c787b0fe2b06f2395393331c95b
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-02-23 15:59:21 +0500

    Update esxi-nic-info.sh

--- hardware/esxi/nic/scripts/esxi-nic-info.sh
+++ hardware/esxi/nic/scripts/esxi-nic-info.sh
@@ -5,21 +5,21 @@
 
 # Get variables
 HVUID=$1
-ADAPID=$2
+NICID=$2
 HOST=$(grep -w $HVUID /etc/zabbix/scripts/hosts-uid-map.txt | awk '{print $NF}')
 
 # Get controller id
 # If the controller id is not passed, we display all the information
-    if [[ $ADAPID = "" ]]; then
+    if [[ $NICID = "" ]]; then
        echo "ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST 'esxcli --debug --formatter=json network nic list'" | bash
     else
 # Integer test value of a variable
     re='^[A-Za-z0-9]+$'
-    if ! [[ $ADAPID =~ $re ]] ; then
+    if ! [[ $NICID =~ $re ]] ; then
        echo "error: Not correct name" >&2; exit 1
     else
 # We form the data of the received controller
-        ADAPSTR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST 'esxcli --debug --formatter=json network nic get -n $NICID'"
-        echo $ADAPSTR | bash
+        STR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST 'esxcli --debug --formatter=json network nic get -n $NICID'"
+        echo $STR | bash
     fi
 fi
