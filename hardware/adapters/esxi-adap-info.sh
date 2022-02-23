8900f4002e3c861e09b5e3b0ca64f677fed6ca3e
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-02-23 06:31:25 +0500

    Update and rename network/esxi/esxi-adap-info.sh to hardware/adapters/esxi-adap-info.sh

---
+++ hardware/adapters/esxi-adap-info.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Collecting data from LSI MegaRAID controller
+# Andrey Kuznetsov, 2022.02.20
+
+# Get variables
+HVUID=$1
+CTLID=$2
+HOST=$(grep -w $HVUID /etc/zabbix/scripts/hosts-uid-map.txt | awk '{print $NF}')
+
+# Get controller id
+# If the controller id is not passed, we display all the information
+    if [[ $CTLID = "" ]]; then
+       echo "ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST '/opt/lsi/storcli/storcli show all j'" | bash
+    else
+# Integer test value of a variable
+    re='^[0-9]+$'
+    if ! [[ $CTLID =~ $re ]] ; then
+       echo "error: Not a number" >&2; exit 1
+    else
+# We form the data of the received controller
+        CTLSTR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST '/opt/lsi/storcli/storcli /c$CTLID show all j'"
+        echo $CTLSTR | bash
+    fi
+fi
