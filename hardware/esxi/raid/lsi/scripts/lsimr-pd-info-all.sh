365f7d195d6be58ebb4fc56adf9503c38cc57098
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-04-18 08:45:57 +0500

    Update lsimr-pd-info-all.sh

--- hardware/esxi/raid/lsi/scripts/lsimr-pd-info-all.sh
+++ hardware/esxi/raid/lsi/scripts/lsimr-pd-info-all.sh
@@ -6,8 +6,8 @@
 # Get variables
 HVUID=$1
 PDNAME=$2
-HOST=$(grep -w $HVUID /etc/zabbix/scripts/hosts-uid-map.txt | awk '{print $NF}')
+HOST=$(grep -w $HVUID /usr/lib/zabbix/internalscripts/hardware/esxi/hosts-uid-map.txt | awk '{print $NF}')
 
 # We form the data of the received controller
-CTLSTR="ssh -i /var/lib/zabbix/.ssh/id_rsa zabbix@$HOST '/opt/lsi/storcli/storcli $PDNAME show all j'"
+CTLSTR="ssh -i /usr/lib/zabbix/.ssh/id_rsa zabbix@$HOST '/opt/lsi/storcli/storcli $PDNAME show all j'"
 echo $CTLSTR | bash
